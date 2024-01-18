2b45f5e0da44cc83cc18b31e1aec5fefeb225835
Eyad Muhammad <eyadmuhammad90@gmail.com>
2024-01-18 21:58:39 +0200

    fixed bugs

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const navToggel = document.querySelector("#menu-btn");
 
 navToggel.addEventListener("click", function () {
-  if (document.querySelector("aside").classList.contains("-right-[100vw]")) {
-    document.querySelector("aside").classList.remove("-right-[100vw]");
+  if (document.querySelector("aside").classList.contains("-right-full")) {
+    document.querySelector("aside").classList.remove("-right-full");
     document.querySelector("aside").classList.add("right-0");
+    document.querySelector('body').style.overflow='hidden'
     this.children[0].name = "close-outline";
   } else {
     document.querySelector("aside").classList.remove("right-0");
-    document.querySelector("aside").classList.add("-right-[100vw]");
+    document.querySelector("aside").classList.add("-right-full");
+    document.body.classList.remove("overflow-hidden",'h-screen');
+    document.querySelector('body').style.overflow='unset'
+
     this.children[0].name = "menu-outline";
   }
 });
